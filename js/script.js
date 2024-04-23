6031d0dad0096a009980c9e5f519cdb41e9db619
GusTate <152634653+GustheGeneral@users.noreply.github.com>
2024-04-23 00:29:41 +0000

    Changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function hotOrCold(temperature) {
+  if (temperature < 15) {
+    return "Cold";
+  } else {
+    return "Hot";
+  }
+}
+
+function calculateTemperature() {
+  let temperatureInput = document.getElementById("temperature").value;
+  let temperature = parseFloat(temperatureInput);
+  let resultElement = document.getElementById("result");
+
+  if (!isNaN(temperature)) {
+    let result = hotOrCold(temperature);
+    resultElement.textContent = `The temperature is ${result}.`;
+  } else {
+    resultElement.textContent = "Please enter a valid number for temperature.";
+  }
+}
